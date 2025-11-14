ae63fdbc5a415cd8a1b5f2254c8c70e88869b241
Juan Carlos Collao Mamani <jccm0909@gmail.com>
2025-11-14 14:19:05 -0400

    First Commit

---
+++ db/connection.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def connect_db():
+    try:
+        conn = mysql.connector.connect(
+            host = os.getenv('DB_HOST'),
+            user = os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            database = os.getenv('DB_NAME')
+        )
+        print("Conexión exitosa a la base de datos")
+        return conn
+    except Exception as e:
+        print(f"Error al conectar con la base de datos: {e}")
+        return None
+
+def close_db(conn):
+    if conn:
+        conn.close()
+
+connect_db()
